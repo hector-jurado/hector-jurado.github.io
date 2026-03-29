99f78d75ef9b74192da06f36a04aa3d69142be2c
Hector <104199317+hector-jurado@users.noreply.github.com>
2026-03-29 13:42:37 +0200

    Implement fade-in effect on scroll
    Add scroll event listener to fade in elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+const elements = document.querySelectorAll(".fade-in")
+    window.addEventListener("scroll", () => {
+    elements.forEach(el => {
+      const position = el.getBoundingClientRect().top;
+      const screenPosition = window.innerHeight /1.3;
 
+      if(position < screenPosition){
+        el.classList.add("visible");
+      }
+    });
+    });
